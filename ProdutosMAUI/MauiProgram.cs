60da0af6bb78e9516051ac14027a71e4ac2c10a2
Vasco Duarte <vascoduarte2004@gmail.com>
2026-01-05 04:12:05 +0000

    Tentativa MAUI

--- ProdutosMAUI/MauiProgram.cs
+++ ProdutosMAUI/MauiProgram.cs
@@ -1,9 +1,14 @@
 ﻿using Microsoft.Extensions.Logging;
 using RCLAPI.Services;
+using RCLComum.Services;
+using Microsoft.AspNetCore.Components.Authorization; 
 
-namespace ProdutosMAUI {
-    public static class MauiProgram {
-        public static MauiApp CreateMauiApp() {
+namespace ProdutosMAUI
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
@@ -13,16 +18,23 @@ namespace ProdutosMAUI {
 
             builder.Services.AddMauiBlazorWebView();
 
-            builder.Services.AddScoped(sp => new HttpClient {
-                BaseAddress = new Uri("https://localhost:7001/") 
-            });
 
+            builder.Services.AddScoped(sp => new HttpClient
+            {
+                BaseAddress = new Uri("https://localhost:7001/")
+            });
+            builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+            builder.Services.AddScoped<AuthService>();
             builder.Services.AddScoped<IProdutoService, ProdutoService>();
+            builder.Services.AddScoped<IEncomendaService, EncomendaService>();
+            builder.Services.AddScoped<CarrinhoService>();
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddCascadingAuthenticationState();
+
 #if DEBUG
             builder.Services.AddBlazorWebViewDeveloperTools();
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
-
             return builder.Build();
         }
     }
